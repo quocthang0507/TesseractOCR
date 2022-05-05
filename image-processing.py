fb44dd37d4cad2e10800dce6f2b9eaa58b259eb7
La Quốc Thắng <quocthang_0507@yahoo.com.vn>
2022-05-05 17:17:26 +0700

    Add new file

---
+++ image-processing.py
@@ -0,0 +1,26 @@
+import pytesseract
+import matplotlib.pyplot as plt
+import cv2
+import os
+
+# Read the license plate file and display it
+test_license_plate = cv2.imread(os.getcwd() + "/license-plates / GWT2180.jpg")
+plt.imshow(test_license_plate)
+plt.axis('off')
+plt.title('GWT2180 license plate')
+
+resize_test_license_plate = cv2.resize(
+    test_license_plate, None, fx=2, fy=2,
+    interpolation=cv2.INTER_CUBIC)
+
+grayscale_resize_test_license_plate = cv2.cvtColor(
+    resize_test_license_plate, cv2.COLOR_BGR2GRAY)
+
+gaussian_blur_license_plate = cv2.GaussianBlur(
+    grayscale_resize_test_license_plate, (5, 5), 0)
+
+new_predicted_result_GWT2180 = pytesseract.image_to_string(gaussian_blur_license_plate, lang='eng',
+                                                           config='--oem 3 -l eng --psm 6 -c tessedit_char_whitelist = ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
+filter_new_predicted_result_GWT2180 = "".join(
+    new_predicted_result_GWT2180.split()).replace(":", "").replace("-", "")
+print(filter_new_predicted_result_GWT2180)
